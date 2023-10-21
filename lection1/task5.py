de11a30a3fd36f9c6d180dafec0e321f3cf0d0c0
adolynko <dolynkoanton@gmail.com>
2023-10-21 09:16:41 +0300

    task 5 done

---
+++ lection1/task5.py
@@ -0,0 +1,18 @@
+#ax^2+bx+c = 0
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(f"{a}x**2+{b}x+{c}=0")
+
+D = b**2 - 4 * a * c
+print("discriminant :",D)
+if D > 0:
+    x1 = (-1 * b) - (D ** (1/2))
+    x2 = (-1 * b) + (D ** (1/2))
+    print("answers : x1 = {answer1}, {answer2}".format(answer1 = x1,answer2 = x2))
+elif D == 0:
+    x = (-1 * b) - (D ** (1/2))
+    print("D = 0. One answer : ",x)
+else:
+    print("D < 0", D)
