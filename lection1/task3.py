c4044751ffda505d4f30d3c1f719ac6a0ede918b
adolynko <dolynkoanton@gmail.com>
2023-11-11 19:29:30 +0200

    simplified user input

--- lection1/task3.py
+++ lection1/task3.py
@@ -1,8 +1,8 @@
-temperature = input("Choose Celsius or fahrenheit : ").lower()
+temperature = input("Type 1)Celsius or 2)Fahrenheit : ")
 degrees = int(input("input degrees : "))
-if temperature == "celsius":
+if temperature == 1:
     print(degrees * (9/5) + 32)
-elif temperature == "fahrenheit":
+elif temperature == 2:
     print((degrees - 32)* 5/9)
 else:
     print("isn`t celsius or fahrenheit")
